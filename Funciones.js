e109b7e9c3480c8c178d93c99c5452eaf21bcecd
Agus Bes7 <aguscabj10.ab@gmail.com>
2024-11-15 00:34:45 -0300

    header,footer,baner,navegacion encaminado, div contenido para cada seccio, hojas de estilos comunes y de la seccion contenido

--- Funciones.js
+++ Funciones.js
@@ -1,21 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Asignamos los elementos de cambio de vista
-    const showRegister = document.getElementById("show-register");
-    const showLogin = document.getElementById("show-login");
-
-    // Formulario de Login y Registro
-    const loginSection = document.querySelector(".login");
-    const registerSection = document.querySelector(".register");
-
-    if (showRegister && showLogin && loginSection && registerSection) {
-        showRegister.addEventListener("click", function() {
-            loginSection.style.display = "none";
-            registerSection.style.display = "flex";
-        });
-
-        showLogin.addEventListener("click", function() {
-            registerSection.style.display = "none";
-            loginSection.style.display = "flex";
-        });
-    }
-});
+        function cambiarGrilla() {
+            // Obtén el contenedor de la grilla y el aside
+            var container = document.querySelector('.contenido');
+            var aside = document.querySelector('.aside');
+            var toggleButton = document.getElementById('toggleButton');
+        
+            // Alterna la clase 'hidden' en el aside
+            aside.classList.toggle('hidden'); 
+        
+            // Alterna la clase 'cambiada' en el contenedor
+            container.classList.toggle('cambiada');
+        
+            // Cambia el texto del botón dependiendo de si el aside está visible o no
+            if (aside.classList.contains('hidden')) {
+                toggleButton.innerHTML = '>'; // Cuando el aside está oculto, el botón muestra '>'
+                toggleButton.classList.add('hidden');
+            } else {
+                toggleButton.innerHTML = '<'; // Cuando el aside está visible, el botón muestra '<'
+                toggleButton.classList.remove('hidden');
+            }
+        }
+        
