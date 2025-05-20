b307786e4e76fc990ed00173af2d2a05b6658f2b
Agus Bes7 <aguscabj10.ab@gmail.com>
2025-05-20 20:11:12 -0300

    añadiendo metodo en formulario de productos

--- controllers/FormularioProductController.js
+++ controllers/FormularioProductController.js
@@ -4,6 +4,22 @@ create: (req,res) =>{
 
     res.render('gestiondeProductos',{})
 },
+ store: (req, res) => {
+    const datos = req.body; // Acá recibís los datos del formulario
+    console.log("Datos recibidos del formulario:", datos);
+
+    res.send("Datos recibidos: " + JSON.stringify(datos)); // Mejor mostrarlo como string
+  
+/*
+value="1">Juego</option>
+<option value="2">Libro</option>
+<option value="3">Artículo*/
+
+
+
+ res.render('gestiondeProductos',{})
+
+},
 updateDestroy: (req,res) =>{
     const index=req.params.id;
        const listaProductos = Array.from(item.items);
