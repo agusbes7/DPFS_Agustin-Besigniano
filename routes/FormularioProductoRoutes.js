0f89bea9a2964be7d44098dd399c21f27bda12e8
Agus Bes7 <aguscabj10.ab@gmail.com>
2025-05-20 20:12:23 -0300

    añadiendo metodos  en formulario de productos

--- routes/FormularioProductoRoutes.js
+++ routes/FormularioProductoRoutes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const form=require('../controllers/FormularioProductController');
 
+router.post('/admin/ABMproductos' ,form.store);
 router.get('/admin/ABMproductos' ,form.create);
+
 router.get('/admin/ABMproductos/:id' ,form.updateDestroy);
 
 module.exports=router;
